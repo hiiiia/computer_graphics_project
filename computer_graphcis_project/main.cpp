18ffdc6cd26d7f5ce32149835fc3aa6e5fac4a6e
hiiia <gksqlc0406@gmail.com>
2023-11-18 17:13:44 +0900

    Update main.cpp

--- computer_graphcis_project/main.cpp
+++ computer_graphcis_project/main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <GL/GL.h>
+#include <GL/GLU.h>
+#include <GL/glut.h>
+void MyDisplay() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glVertex3f(-0.5f, -0.5f, 0.0f);
+	glVertex3f(0.5f, -0.5f, 0.0f);
+	glVertex3f(0.5f, 0.5f, 0.0f);
+	glVertex3f(-0.5f, 0.5f, 0.0f);
+	glEnd();
+	glFlush();
+}
 
-using namespace std;
-
-int main()
-{
-	cout << "HI";
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutCreateWindow("Example");
+	glutDisplayFunc(MyDisplay);
+	glutMainLoop();
+	return 0;
 }
