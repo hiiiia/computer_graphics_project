c6ac2a3684b3b48a2d9744c15e46f1b3caea5fe5
hiiia <gksqlc0406@gmail.com>
2023-11-20 21:50:10 +0900

    d
    d

--- computer_graphcis_project/main.cpp
+++ computer_graphcis_project/main.cpp
@@ -1,137 +1,152 @@
-//#include <GL/glut.h>
-//#include <GL/gl.h>
-//#include <GL/glu.h>
-//
-//static int SpinAngle = 0;
-//
-//void InitLight() {
-//    GLfloat light0_ambient[] = { 0.5, 0.4, 0.3, 1.0 };     //조명 특성
-//    GLfloat light0_diffuse[] = { 0.8, 0.7, 0.6, 1.0 };
-//    GLfloat light0_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-//
-//    GLfloat material_ambient[] = { 0.4, 0.4, 0.4, 1.0 };  //물체 특성
-//    GLfloat material_diffuse[] = { 0.9, 0.9, 0.9, 1.0 };
-//    GLfloat material_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-//    GLfloat material_shininess[] = { 25.0 };
-//
-//    glShadeModel(GL_SMOOTH);    //구로 셰이딩
-//    glEnable(GL_DEPTH_TEST);    //깊이 버퍼 활성화
-//    glEnable(GL_LIGHTING);      //조명 활성화
-//    glEnable(GL_LIGHT0);
-//    glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
-//    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
-//    glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
-//}
-//
-//void MyDisplay() {
-//    GLfloat LightPosition[] = { 0.0, 0.0, 1.5, 1.0 };
-//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//    glPushMatrix();
-//    glTranslatef(0.0, 0.0, -5.0);
-//    glPushMatrix();
-//    glRotatef(SpinAngle, 1.0, 0.0, 0.0);
-//    glLightfv(GL_LIGHT0, GL_POSITION, LightPosition);
-//    glTranslatef(0.0, 0.0, 1.5);
-//    glDisable(GL_LIGHTING);
-//    glColor3f(0.9, 0.9, 0.9);
-//    glutWireSphere(0.06, 10, 10);
-//    glEnable(GL_LIGHTING);
-//    glPopMatrix();
-//    glutSolidSphere(1.0, 400, 400);
-//    glPopMatrix();
-//    glFlush();
-//}
-//
-//void MyReshape(int w, int h) {
-//    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-//    glMatrixMode(GL_PROJECTION);
-//    glLoadIdentity();
-//    gluPerspective(40.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
-//}
-//
-//void MyMouse(int button, int state, int x, int y) {
-//    switch (button) {
-//    case GLUT_LEFT_BUTTON:
-//        if (state == GLUT_DOWN) {
-//            SpinAngle = (SpinAngle + 15) % 360;
-//            glutPostRedisplay();
-//        }
-//        break;
-//    default:
-//        break;
-//    }
-//}
-//
-//int main(int argc, char** argv) {
-//    glutInit(&argc, argv);
-//    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-//    glutInitWindowSize(500, 500);
-//    glutInitWindowPosition(100, 100);
-//    glutCreateWindow("openGL Sample Drawing");
-//    InitLight();
-//    glutDisplayFunc(MyDisplay);
-//    glutReshapeFunc(MyReshape);
-//    glutMouseFunc(MyMouse);
-//    glutMainLoop();
-//    return 0;
-//}
-
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
-#define GLEW_STATIC
-#include <GL/glew.h>
+#include <vector>
+
+#include <glm/glm.hpp> 
+#include <glm/gtc/matrix_transform.hpp> 
+#include <glm/gtc/type_ptr.hpp>
+using namespace glm;
 
 
-#define WIDTH   4
-#define HEIGHT  4
 
-GLubyte MyTexture[WIDTH][HEIGHT][3];
+//define camera class
+class camera {
 
-void FillMyTexture() {
-    int s, t;
-    for (s = 0; s < WIDTH; s++) {
-        for (t = 0; t < HEIGHT; t++) {
-            GLubyte Intensity = ((s + t) % 2) * 255;    //0는 흑색, 255는 백색
-            MyTexture[s][t][0] = Intensity;             //Red 값에 할당
-            MyTexture[s][t][1] = Intensity;             //Green 값에 할당
-            MyTexture[s][t][2] = Intensity;             //Blue 값에 할당
-        }
+public:
+    //variables for opengl
+    vec3 eye; //center position
+    vec3 at; //point where this camera is pointing out
+    vec3 up; // up direction (normalized)
+
+    //other variables
+    vec3 forward; //vector pointing forward direction
+    float distance = 1; //distance from eye to at
+    vec3 right; //vector indicating right direction from the point of camera
+
+    void InitCamera(vec3 _center, vec3 _at, vec3 _up)
+    {
+        //set three variables and update others
+        eye = _center;
+        at = _at;
+        up = _up;
+        UpdateCamera();
+    }
+
+    void MoveCamera(vec3 move)
+    {
+        eye += move;
+        at += move;
+    }
+
+    void RotateCamera(vec3 axis, float angle)
+    {
+        //rotate direc and up vector
+        vec4 tempForward = vec4(forward, 0);
+        vec4 tempUp = vec4(up, 0);
+        mat4 rot = rotate(mat4(1), radians(angle), axis);
+        forward = rot * tempForward;
+        up = rot * tempUp;
+
+        //update at vector accordingly
+        at = eye + forward * distance;
+
+        UpdateCamera();
+    }
+
+    //update variables accordingly
+    void UpdateCamera()
+    {
+        up = normalize(up);
+        distance = length(at - eye);
+        forward = normalize(at - eye);
+        right = normalize(cross(forward, up));
+    }
+};
+
+camera myCamera;
+
+vec2 preMouse, currentMouse;
+int windowHeight, windowWidth;
+
+static int SpinAngle = 0;
+
+
+
+
+///
+GLfloat mat_diffuse[] = { 0.25, 0.25, 1., 0. };  //확산반사
+GLfloat mat_specular[] = { 1., 1., 1., 0. };      //경면반사
+GLfloat light_position[] = { 10., 10., 20., 1. };   //광원의 위치
+GLfloat ref_plane[] = { 1.5, 1.5, 1.5, 0. };   //텍스쳐 기준평면
+GLUquadricObj* qobj;                                //물체 포인터
+unsigned int MyTextureObject;                       //텍스쳐 객체면
+
+#define stripeImageWidth    32
+GLubyte stripeImage[4 * stripeImageWidth];          //텍스쳐 배열
+
+void MyStripeImage() {                              //텍스쳐 생성함수
+    for (int j = 0; j < stripeImageWidth; j++) {
+        stripeImage[4 * j] = 255;
+        stripeImage[4 * j + 1] = (j < 8) ? 0 : 255;
+        stripeImage[4 * j + 2] = (j < 8) ? 0 : 255;
+        stripeImage[4 * j + 3] = 0;
     }
 }
 
-void MyInit() {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    FillMyTexture();
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, WIDTH, HEIGHT, 0, GL_RGB,
-        GL_UNSIGNED_BYTE, &MyTexture[0][0][0]);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-    glEnable(GL_TEXTURE_2D);
-}
-
 void MyDisplay() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, 0.0);
-    glTexCoord2f(0.0, 3.0); glVertex3f(-1.0, 1.0, 0.0);
-    glTexCoord2f(3.0, 3.0); glVertex3f(1.0, 1.0, 0.0);
-    glTexCoord2f(3.0, 0.0); glVertex3f(1.0, -1.0, 0.0);
-    glEnd();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBindTexture(GL_TEXTURE_1D, MyTextureObject);
+    gluSphere(qobj, 1.5, 40, 40);
     glutSwapBuffers();
 }
 
-int main(int argc, char** argv) {
+void Init() {
+    qobj = gluNewQuadric();
+    gluQuadricDrawStyle(qobj, GLU_FILL);
+
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialf(GL_FRONT, GL_SHININESS, 25.0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_SMOOTH);
+
+    MyStripeImage();
+    glGenTextures(1, &MyTextureObject);
+    glBindTexture(GL_TEXTURE_1D, MyTextureObject);
+    glTexImage1D(GL_TEXTURE_1D, 0, 4, stripeImageWidth, 0,
+        GL_RGBA, GL_UNSIGNED_BYTE, stripeImage);
+    glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+    glTexGenfv(GL_S, GL_OBJECT_PLANE, ref_plane);
+    glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    //glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    glEnable(GL_TEXTURE_GEN_S);
+    glEnable(GL_TEXTURE_1D);
+}
+
+void MyReshape(int w, int h) {
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(40., (GLfloat)w / (GLfloat)h, 1., 10.);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0., 0., 5., 0., 0., 0., 0., 1., 0.);
+    glTranslatef(0., 0., -1.);
+}
+
+void main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutCreateWindow("openGL Sample Program");
-    MyInit();
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutCreateWindow("openGL sample program");
+    glutReshapeFunc(MyReshape);
     glutDisplayFunc(MyDisplay);
+    Init();
     glutMainLoop();
-    return 0;
 }
